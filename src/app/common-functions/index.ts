4dcd1012e509922a1a06e00f0f5726b10175e98d
babluroy <bablu.roy@batchservice.com>
2024-09-26 22:12:19 +0530

    added support for multi delimiters

--- src/app/common-functions/index.ts
+++ src/app/common-functions/index.ts
@@ -11,22 +11,23 @@ export class StringCalculator {
     add(numbers: string): number {
         if (numbers === "") return 0;
 
-        let delimiter = /[\n,]/;
+        let delimiter = /[\n,]/; 
         if (numbers.startsWith("//")) {
             const parts = numbers.split("\n");
-            delimiter = new RegExp(`[${parts[0].slice(2)}]`);
-            numbers = parts[1];
+            const delimiterPart = parts[0].slice(2); 
+            delimiter = new RegExp(`[${delimiterPart}]`); 
+            numbers = parts[1]; 
         }
-
+    
         const numArray = numbers.split(delimiter).map(num => Number(num));
-
+    
         const negatives = numArray.filter(num => num < 0);
         if (negatives.length > 0) {
             throw new Error(`Negative numbers not allowed: ${negatives.join(",")}`);
         }
-
+    
         return numArray.reduce((acc, curr) => acc + curr, 0);
-
     }
+    
 
 }
