bb850dd21b3f2cdb274664602214d79898dc7f92
babluroy <bablu.roy@batchservice.com>
2024-09-26 21:40:02 +0530

    added code to support delimiter and sums

--- src/app/common-functions/index.ts
+++ src/app/common-functions/index.ts
@@ -1,8 +1,19 @@
 
 export class StringCalculator {
 
-    add(numbers: string) {
-      if (numbers === "") return 0;
+    add(numbers: string):number {
+        if (numbers === "") return 0;
+
+        let delimiter = /[\n,]/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n");
+            delimiter = new RegExp(`[${parts[0].slice(2)}]`);
+            numbers = parts[1];
+        }
+
+        const numArray = numbers.split(delimiter).map(num => Number(num));
+        return numArray.reduce((acc, curr) => acc + curr, 0);
+
     }
-  
-  }
+
+}
