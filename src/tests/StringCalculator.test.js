b59d5e7b2f9ce9f2171d66cb0e7523c4b405297f
babluroy <bablu.roy@batchservice.com>
2024-09-26 21:41:23 +0530

    added tests for sums of digits including custom delimiter

--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -15,4 +15,21 @@ describe('String Calculator', () => {
         expect(calculator.add("1")).toBe(1);
     });
 
+    test('returns 6 for input "1,5"', () => {
+        expect(calculator.add("1,5")).toBe(6);
+    });
+
+    test('returns 10 for input "1,2,3,4"', () => {
+        expect(calculator.add("1,2,3,4")).toBe(10);
+    });
+
+    test('returns 6 for input "1\\n2,3"', () => {
+        expect(calculator.add("1\n2,3")).toBe(6);
+    });
+
+    test('returns 3 for input "//;\\n1;2"', () => {
+        expect(calculator.add("//;\n1;2")).toBe(3);
+    });
+
+
 });
