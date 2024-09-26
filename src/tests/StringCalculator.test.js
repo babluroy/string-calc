376d68a162429fae99bc236af6763f21230213ea
babluroy <bablu.roy@batchservice.com>
2024-09-26 21:42:05 +0530

    test for negative number

--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -31,5 +31,8 @@ describe('String Calculator', () => {
         expect(calculator.add("//;\n1;2")).toBe(3);
     });
 
+    test('throws an error for negative numbers', () => {
+        expect(() => calculator.add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+      });    
 
 });
