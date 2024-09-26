04d2c82a5dc547b849b624a9e0ca794154614a52
babluroy <bablu.roy@batchservice.com>
2024-09-26 21:33:11 +0530

    test for empty string

--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -1,5 +1,14 @@
 const { StringCalculator } = require('../app/common-functions');
 
 describe('String Calculator', () => {
+    let calculator;
 
+    beforeEach(() => {
+        calculator = new StringCalculator();
+    });
+
+    test('returns 0 for an empty string', () => {
+        expect(calculator.add("")).toBe(0);
+    });
+    
 });
